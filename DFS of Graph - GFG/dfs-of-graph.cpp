1020b651fb33244475df59f822a7f265939e2c59
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-06-08 10:39:58 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    
+    void dfs(int i,vector<int> adj[],unordered_map<int,bool>&visited,vector<int>&ans){
+        
+      visited[i]=true;
+      ans.push_back(i);
+      for(auto j:adj[i]){
+        if(!visited[j]){
+            dfs(j,adj,visited,ans);
+        }  
+      }
+    
+    }
+    
+    
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>ans;
+    unordered_map<int,bool>visited;
+        
+       
+                dfs(0,adj,visited,ans);
+            
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
