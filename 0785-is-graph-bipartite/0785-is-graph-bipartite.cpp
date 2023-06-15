95a4025e1c9d5b3441064f1723d3b6636d8cd609
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-06-15 11:38:52 +0530

    Time: 26 ms (70.27%), Space: 13.3 MB (99.56%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     
-    bool check(int node,vector<vector<int>>&graph,int color[]){
-        color[node]=0;
-        queue<int>q;
-        q.push(node);
-        while(!q.empty()){
-            int a=q.front();
-            q.pop();
-            for(auto nei:graph[a]){
-                if(color[nei]==-1){
-                    color[nei]=!color[a];
-                    q.push(nei);
-                }else if(color[a]==color[nei]){
+//     bool check(int node,vector<vector<int>>&graph,int color[]){
+//         color[node]=0;
+//         queue<int>q;
+//         q.push(node);
+//         while(!q.empty()){
+//             int a=q.front();
+//             q.pop();
+//             for(auto nei:graph[a]){
+//                 if(color[nei]==-1){
+//                     color[nei]=!color[a];
+//                     q.push(nei);
+//                 }else if(color[a]==color[nei]){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+        
+//     }
+    
+    
+    bool dfs(int node,int c,vector<vector<int>>& graph,int color[]){
+        color[node]=c;
+        for(auto nei:graph[node]){
+            if(color[nei]==-1){
+                if(dfs(nei,!c,graph,color)==false){
                     return false;
                 }
+            }else if(color[nei]==color[node]){
+                return false;
             }
         }
         return true;
-        
     }
-    
     
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
@@ -30,7 +44,7 @@ public:
         }
         for(int i=0;i<V;i++){
             if(color[i]==-1){
-                if(check(i,graph,color)==false){
+                if(dfs(i,0,graph,color)==false){
                     return false;
                 }
             }
