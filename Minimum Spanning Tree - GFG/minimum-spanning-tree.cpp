307211075fa75f412a60f8dd7e7e569790205456
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-06-19 10:44:39 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -62,45 +62,130 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
+    // int spanningTree(int V, vector<vector<int>> adj[])
+    // {
         
-        int sum=0;
-        vector<int>key(V,INT_MAX);
-        vector<int>parent(V,-1);
-        vector<bool>mst(V,false);
-        key[0]=0;
-        mst[0]=true;
-        parent[0]=0;
-        for(int i=0;i<V;i++){
-            int mini=INT_MAX;
-            int u;
-            for(int j=0;j<V;j++){
-                if(mst[j]==false&&key[j]<mini){
+    //     int sum=0;
+    //     vector<int>key(V,INT_MAX);
+    //     vector<int>parent(V,-1);
+    //     vector<bool>mst(V,false);
+    //     key[0]=0;
+    //     mst[0]=true;
+               
+    //     for(int i=0;i<V;i++){
+    //         int mini=INT_MAX;
+    //         int u;
+    //         for(int j=0;j<V;j++){
+    //             if(mst[j]==false&&key[j]<mini){
                    
-                    u=j;
-                    mini=key[j];
-                }
-            }
-             mst[u]=true;
-            for(auto nei:adj[u]){
-                int v=nei[0];
-                int w=nei[1];
+    //                 u=j;
+    //                 mini=key[j];
+    //             }
+    //         }
+    //          mst[u]=true;
+    //         for(auto nei:adj[u]){
+    //             int v=nei[0];
+    //             int w=nei[1];
                 
-                if(mst[v]==false&&w<key[v]){
-                    parent[v]=u;
-                    key[v]=w;
-                }
+    //             if(mst[v]==false&&w<key[v]){
+    //                 parent[v]=u;
+    //                 key[v]=w;
+    //             }
                 
-            }
+    //         }
             
             
-        }
+    //     }
         
+    //     for(int i=0;i<V;i++){
+    //             sum+=key[i];
+    //         }
+    //         return sum;
+    // }
+    // int spanningTree(int V, vector<vector<int>> adj[]){
+    //     vector<int>parent(V,-1);
+    //     vector<int>key(V,INT_MAX);
+    //     vector<bool>mst(V,false);
+    //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    //     pq.push({0,0});
+    //     key[0]=0;
+    //     parent[0]=0;
+    //     int sum=0;
+    //     while(!pq.empty()){
+    //         int u=pq.top().second;
+    //         pq.pop();
+            
+    //          if(mst[u]==true){
+    //                 continue;
+    //             }
+                
+    //         mst[u]=true;    
+    //         sum+=key[u];
+    //         for(auto nei:adj[u]){
+               
+                
+    //             int weight=nei[1];
+    //             int v=nei[0];
+    //             if(mst[v]==false&&weight<key[v]){
+    //                 key[v]=weight;
+    //                 parent[v]=u;
+    //                 pq.push({key[v],v});
+    //             }
+    //         }
+            
+            
+    //     }
+    //     return sum;
+    // }
+    
+    int findparent(vector<int>&parent,int node){
+        if(parent[node]==node){
+            return parent[node];
+        }
+        return parent[node]=findparent(parent,parent[node]);
+    }
+    void unionset(int u,int v,vector<int>&parent,vector<int>&rank){
+        
+        int pu=findparent(parent,u);
+        int pv=findparent(parent,v);
+        if(pu==pv){
+            return;
+        }
+        if(rank[v]>rank[u]){
+            parent[u]=v;
+        }else if(rank[u]>rank[v]){
+            parent[v]=u;
+        }else{
+            parent[u]=v;
+            rank[v]++;
+        }
+    }
+    
+    int spanningTree(int V, vector<vector<int>> adj[]){
+        vector<pair<int,pair<int,int>>>edg;
         for(int i=0;i<V;i++){
-                sum+=key[i];
+            for(auto it:adj[i]){
+                int u=it[0];
+                int wt=it[1];
+                edg.push_back({wt,{i,u}});
             }
-            return sum;
+        }
+        sort(edg.begin(),edg.end());
+        vector<int>parent(V);
+        vector<int>rank(V,0);
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+        }
+        int minweight=0;
+        for(auto it:edg){
+            int u=findparent(parent,it.second.first);
+            int v=findparent(parent,it.second.second);
+            if(u!=v){
+                minweight+=it.first;
+                unionset(u,v,parent,rank);
+            }
+        }
+        return minweight;
     }
 };
 
