befbe8afb181f442db066d04ec95e764afb3d73b
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-07-11 16:01:50 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -10,30 +10,51 @@ using namespace std;
 class Solution{   
 public:
 
-    bool solve(int n,int sum,vector<int>&arr,vector<vector<int>>&dp){
-        if(sum==0){
-            return 1;
-        }
-        if(n==0){
-            return sum==arr[0];
-        }
+    // bool solve(int n,int sum,vector<int>&arr,vector<vector<int>>&dp){
+    //     if(sum==0){
+    //         return 1;
+    //     }
+    //     if(n==0){
+    //         return sum==arr[0];
+    //     }
         
-        if(dp[n][sum]!=-1){
-            return dp[n][sum];
-        }
+    //     if(dp[n][sum]!=-1){
+    //         return dp[n][sum];
+    //     }
         
-        bool nott=solve(n-1,sum,arr,dp);
-        bool take=false;
-        if(sum>=arr[n]){
-            take=solve(n-1,sum-arr[n],arr,dp);
-        }
-        return dp[n][sum]=take||nott;
-    }
+    //     bool nott=solve(n-1,sum,arr,dp);
+    //     bool take=false;
+    //     if(sum>=arr[n]){
+    //         take=solve(n-1,sum-arr[n],arr,dp);
+    //     }
+    //     return dp[n][sum]=take||nott;
+    // }
 
-    bool isSubsetSum(vector<int>arr, int sum){
+    bool isSubsetSum(vector<int>arr, int k){
         int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return solve( n-1,sum,arr,dp);
+       vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = true;
+    }
+    
+    if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            bool notTaken = dp[ind-1][target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+        
+            dp[ind][target]= notTaken||taken;
+        }
+    }
+    
+    return dp[n-1][k];
     }
 };
 
