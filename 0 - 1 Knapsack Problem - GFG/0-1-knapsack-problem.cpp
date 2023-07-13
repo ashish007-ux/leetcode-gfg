ab4b6573505184e45ad3b9234d03ce8f72c29c5d
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-07-13 09:35:00 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -9,35 +9,58 @@ class Solution
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     
-    int solve(int n,int W,int val[],int arr[],vector<vector<int>>&dp){
-        if(n==0){
-            if(arr[0]<=W){
-                return val[0];
-            }else{
-                return 0;
-            }
-        }
+    // int solve(int n,int W,int val[],int arr[],vector<vector<int>>&dp){
+    //     if(n==0){
+    //         if(arr[0]<=W){
+    //             return val[0];
+    //         }else{
+    //             return 0;
+    //         }
+    //     }
         
-        if(dp[n][W]!=-1){
-            return dp[n][W];
-        }
+    //     if(dp[n][W]!=-1){
+    //         return dp[n][W];
+    //     }
         
         
-        int nottake=solve(n-1,W,val,arr,dp);
-        int take=INT_MIN;
-        if(W>=arr[n]){
-            take=val[n]+solve(n-1,W-arr[n],val,arr,dp);
-        }
-        return dp[n][W]=max(nottake,take);
-    }
+    //     int nottake=solve(n-1,W,val,arr,dp);
+    //     int take=INT_MIN;
+    //     if(W>=arr[n]){
+    //         take=val[n]+solve(n-1,W-arr[n],val,arr,dp);
+    //     }
+    //     return dp[n][W]=max(nottake,take);
+    // }
     
     
     
     int knapSack(int W, int wt[], int val[], int n) 
     { 
         
-       vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       return solve(n-1,W,val,wt,dp);
+       vector<vector<int>>dp(n,vector<int>(W+1,0));
+    //   return solve(n-1,W,val,wt,dp);
+    
+    for(int j=wt[0];j<=W;j++){
+        
+            dp[0][j]=val[0];
+        
+    }
+    
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=W;j++){
+            int nt=dp[i-1][j];
+            int tak=INT_MIN;
+            if(j>=wt[i]){
+                tak=val[i]+dp[i-1][j-wt[i]];
+            }
+            
+            dp[i][j]=max(nt,tak);
+        }
+    }
+    
+    return dp[n-1][W];
+    
+    
+    
     }
 };
 
