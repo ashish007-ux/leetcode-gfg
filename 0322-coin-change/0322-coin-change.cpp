cb536ce019afdad4cd3c98887063da4eecb7fd46
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-07-13 10:18:39 +0530

    Time: 164 ms (25.75%), Space: 45 MB (37.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,43 +1,56 @@
 class Solution {
 public:
     
-    int solve(int n,vector<int>&coins,int amount,vector<vector<int>>&dp){
-        //   if(amount==0){
-        //     return 1;
-        //  }
-        // if(amount<0){
-        //     return 1e9;
-        // }
-        if(n==0){
-            if(amount%coins[0]==0){
-                return amount/coins[0];
-            }else{
-                return INT_MAX-3;
-            }
-        }
-        if(dp[n][amount]!=-1){
-            return dp[n][amount];
-        }
-        int nottak=solve(n-1,coins,amount,dp);
-        int take=INT_MAX-3;
-        if(amount>=coins[n]){
-            take=1+solve(n,coins,amount-coins[n],dp);
-        }
+    // int solve(int n,vector<int>&coins,int amount,vector<vector<int>>&dp){
+   
+//         if(n==0){
+//             if(amount%coins[0]==0){
+//                 return amount/coins[0];
+//             }else{
+//                 return INT_MAX-3;
+//             }
+//         }
+//         if(dp[n][amount]!=-1){
+//             return dp[n][amount];
+//         }
+//         int nottak=solve(n-1,coins,amount,dp);
+//         int take=INT_MAX-3;
+//         if(amount>=coins[n]){
+//             take=1+solve(n,coins,amount-coins[n],dp);
+//         }
         
-        return dp[n][amount]=min(nottak,take);
+//         return dp[n][amount]=min(nottak,take);
       
+//     }
+    
+    
+    int coinChange(vector<int>& arr, int T) {
+        
+           
+    int n= arr.size();
+    
+    vector<vector<int>> dp(n,vector<int>(T+1,0));
+    
+    for(int i=0; i<=T; i++){
+        if(i%arr[0] == 0)  
+            dp[0][i] = i/arr[0];
+        else dp[0][i] = 1e9;
     }
     
-    
-    int coinChange(vector<int>& coins, int amount) {
-        
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,coins,amount,dp);
-        if(ans>1e9){
-            return -1;
-        }else{
-            return ans;
+    for(int ind = 1; ind<n; ind++){
+        for(int target = 0; target<=T; target++){
+            
+            int notTake = 0 + dp[ind-1][target];
+            int take = 1e9;
+            if(arr[ind]<=target)
+                take = 1 + dp[ind][target - arr[ind]];
+                
+             dp[ind][target] = min(notTake, take);
         }
+    }
+    
+    int ans = dp[n-1][T];
+    if(ans >=1e9) return -1;
+    return ans;
     }
 };
