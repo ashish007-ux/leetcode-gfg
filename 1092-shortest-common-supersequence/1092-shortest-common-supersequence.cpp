44c65ba2bdf54c1cf551a07c2909b5db7d1cfb7b
ashish007-ux <89539996+ashish007-ux@users.noreply.github.com>
2023-07-14 12:11:03 +0530

    Time: 46 ms (13.18%), Space: 13 MB (35.57%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-        int n=s1.size();
-        int m=s2.size();
-        int t[n+1][m+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0||j==0){
-                    t[i][j]=0;
-                }
-            }
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.size();
+        int m=str2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
         }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(s1[i-1]==s2[j-1]){
-                    t[i][j]=1+t[i-1][j-1];
+        for(int j=0;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
                 }else{
-                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        
         int i=n;
         int j=m;
         string ans="";
         while(i>0&&j>0){
-            if(s1[i-1]==s2[j-1]){
-                ans.push_back(s1[i-1]);
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
                 i--;
                 j--;
-            }else if(t[i][j-1]>t[i-1][j]){
-                  ans.push_back(s2[j-1]);
-                  j--;
+            }else if(dp[i-1][j]>dp[i][j-1]){
+                ans+=str1[i-1];
+                i--;
             }else{
-                   ans.push_back(s1[i-1]);
-                   i--;
+                ans+=str2[j-1];
+                j--;
             }
         }
         while(i>0){
-            ans.push_back(s1[i-1]);
+            ans+=str1[i-1];
             i--;
         }
         while(j>0){
-            ans.push_back(s2[j-1]);
+            ans+=str2[j-1];
             j--;
         }
-         reverse(ans.begin(),ans.end());
-     return ans;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
